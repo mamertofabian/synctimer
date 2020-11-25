ae81496ee7943267e59e1dc1dd457a5be269fc8d
Mamerto Fabian <mfabianjr@gmail.com>
2020-11-25 15:40:35 +0800

    Created header, footer, bootstrap, fa setup

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,19 @@
+import { Container } from "react-bootstrap";
+
 import "./App.css";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
 
 const App = () => {
   return (
     <div>
-      <h1>Welcome to SharedTimer</h1>
+      <Header />
+      <main className="py-3">
+        <Container>
+          <h1>Welcome to SharedTimer</h1>
+        </Container>
+      </main>
+      <Footer />
     </div>
   );
 };
---
+++ frontend/src/components/Header.js
@@ -0,0 +1,24 @@
+import React from "react";
+import { Container, Nav, Navbar } from "react-bootstrap";
+const Header = () => {
+  return (
+    <header>
+      <Navbar bg="dark" variant="dark" expand="lg" collapseOnSelect>
+        <Container>
+          <Navbar.Brand href="/">SharedTimer</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="ml-auto">
+              <Nav.Link href="/">Home</Nav.Link>
+              <Nav.Link href="/login">
+                <i className="fas fa-user"></i> Sign In
+              </Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </header>
+  );
+};
+
+export default Header;
